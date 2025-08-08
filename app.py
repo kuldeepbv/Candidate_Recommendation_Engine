480193a7f4bda8aa6759a2e21c3429a7a9aae7bd
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-08-08 14:16:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.title("Candidate Recommendation Engine")
 
 # --- Inputs ---
 st.subheader("Job Description")
-jd_text = st.text_area(label="placeholder_label", height=240, placeholder="Paste the job description here...")
+jd_text = st.text_area(label="placeholder_label", height=240, placeholder="Paste the job description here...", label_visibility="collapsed")
 uploaded = st.file_uploader("Upload resumes (PDF, DOCX, TXT). Multiple allowed.", type=["pdf","docx","txt"], accept_multiple_files=True)
 
 colA, _ = st.columns([1, 2])
